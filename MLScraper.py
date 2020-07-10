410d9b66eb8c51722cd3f1eb5493f0ac8c19d084
Joseph Wagner <comteam@protonmail.com>
2020-07-10 01:12:15 -0500

    Links to google sheet

--- MLScraper.py
+++ MLScraper.py
@@ -4,7 +4,7 @@ import json
 from dotenv import load_dotenv, find_dotenv
 import os
 from os.path import abspath, join, dirname
-
+import pygsheets
 
 
 
@@ -36,17 +36,22 @@ def get_api_key():
 
 
 
-def api_property_list_for_sale(api_key, city, state, prop_type, limit=200):
+def api_property_list_for_sale(api_key, sqft_min, postal_code, prop_type, limit):
   # url for api
   url = "https://realtor.p.rapidapi.com/properties/v2/list-for-sale"
 
   # enter parameters
   querystring = {
-    "sort":"relevance",
-    "city":city,
+    "sort":"price_low",
+    "is_foreclosure": "false",
+    "is_contingent": "false",
+    "is_new_construction": "false",
+    "is_new_plan": "false",
+    "is_pending": "false",
+    "sqft_min": sqft_min,
     "offset":"0",
     "limit":limit,
-    "state_code":state,
+    "postal_code": postal_code,
     "prop_type":prop_type
   }
 
@@ -122,21 +127,29 @@ def process_list_for_sale_response(response_json):
 
 
 realtor_api_key = get_api_key() # api key to access data
-city = "Chicago"
-state = "IL"
-prop_type = "single-family"
+prop_type = "single_family"
+sqft_min = "2000"
+postal_code = "60647"
+limit = 1
 
 
 
-
-property_list_for_sale_response = api_property_list_for_sale(api_key=realtor_api_key, 
-                                                             city=city, 
-                                                             state=state, 
+property_list_for_sale_response = api_property_list_for_sale(api_key=realtor_api_key, sqft_min= sqft_min, postal_code=postal_code,
                                                              prop_type=prop_type,
-                                                             limit=1)
+                                                             limit=limit)
 
 
 df_properties_for_sale_raw = process_list_for_sale_response(response_json=property_list_for_sale_response)
-df_properties_for_sale_raw.head()
+df_properties_for_sale_raw.head(5)
 
-print(df_properties_for_sale_raw.head())
+client = pygsheets.authorize(service_file='Realtor-viz-data-b5a9fbcd94bf.json')
+print("-----------------Authorized--------------------")
+
+sheet = client.open('SubletInn MLScraper')
+print("-----------------Sheet Opened------------------")
+
+wks = sheet[0]
+print("-----------------First Sheet Accessed----------")
+
+wks.set_dataframe(df_properties_for_sale_raw.head(5),(1,1))
+print("-----------------Data Updated------------------")
