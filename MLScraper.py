54bb9ecf423748b85ac6a2077ae03c770a3879a1
Joseph Wagner <comteam@protonmail.com>
2020-07-17 17:32:42 -0500

    Groups and posts properties with similiar sqft

--- MLScraper.py
+++ MLScraper.py
@@ -102,7 +102,7 @@ def post_to_sheets():
   compareList.set_dataframe(df_compare_list,(1,1))
   print("-----------------Data Updated------------------")
 
-def post_to_comparables_tab():
+def post_to_comparables_tab(group_spacing):
   client = pygsheets.authorize(service_file='Realtor-viz-data-b5a9fbcd94bf.json')
   print("-----------------Authorized--------------------")
 
@@ -112,8 +112,10 @@ def post_to_comparables_tab():
   analyzedList_wks = sheet[1]
   print("-----------------Analyzed Tab Accessed----------")
 
-  analyzedList_wks.set_dataframe(df_properties_for_sale_raw,(1,3))
+  analyzedList_wks.set_dataframe(df_analyzed_list,(group_spacing,1))
   print("-----------------Data Updated------------------")
+
+
 
 
 #---------------------------------------------------------------------------------------------------------------------------
@@ -135,25 +137,56 @@ property_list_for_sale_response = api_property_list_for_sale(api_key=realtor_api
 df_properties_for_sale_raw = process_list_for_sale_response(response_json=property_list_for_sale_response)
 
 
-df_empty_list = pd.DataFrame() #empty dataframe to be used
-df_buy_list = pd.concat([df_empty_list, df_properties_for_sale_raw[df_properties_for_sale_raw['price'] <= 300000]])
-df_compare_list = pd.concat([df_empty_list, df_properties_for_sale_raw[df_properties_for_sale_raw['price'] > 300000]])
+df_buy_list = pd.DataFrame() #empty dataframe to be used
+df_buy_list = pd.concat([df_buy_list, df_properties_for_sale_raw[df_properties_for_sale_raw['price'] <= 300000]])
 
-# Convert dataframes to dict for comparisons
-dict_buy_list = df_buy_list.to_dict()
-dict_compare_list = df_compare_list.to_dict()
+df_compare_list = pd.DataFrame()
+df_compare_list = pd.concat([df_compare_list, df_properties_for_sale_raw[df_properties_for_sale_raw['price'] > 300000]])
 
-
-# TODO: Create for loop to compare buy group to compare group
-
-  
-
-
-
-#---------------------------------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------------------------
-
-print(type(dict_buy_list["price"]))
 
 print('Number of listings found: ', df_properties_for_sale_raw['property_id'].count())
+
+# TODO: Create for loop to create comparable groups
+
+group_spacing = 2
+include_comparables = 1
+
+for row in df_buy_list.itertuples() :
+  df_analyzed_list = pd.DataFrame()
+
+  buyable_property = {'Property ID':row.property_id, 'Web URL':row.rdc_web_url, 'Building SQFT':row.building_size}
+  df_analyzed_list = df_analyzed_list.append(buyable_property, ignore_index=True)
+
+
+  #get buyable building size
+  if 'dict' in str(type(row.building_size))  : #only compares if the field isn't blank
+    for key, value in row.building_size.items() :
+      if key == 'size':
+        buyable_building_size = value
+
+  #get building size from each compare group row
+  for row in df_compare_list.itertuples() :
+    if 'dict' in str(type(row.building_size)) :
+      for key, value in row.building_size.items() :
+        if key == 'size':
+          compared_building_size = value
+      if buyable_building_size - compared_building_size <= 500 and buyable_building_size - compared_building_size >= -500:
+        comparable_property = {'Property ID':row.property_id, 'Web URL':row.rdc_web_url, 'Building SQFT':row.building_size}
+        df_analyzed_list = df_analyzed_list.append(comparable_property, ignore_index=True)
+        include_comparables = include_comparables + 1
+    else:
+      continue    
+      
+ 
+  post_to_comparables_tab(group_spacing)
+  del df_analyzed_list
+  group_spacing = 2 + include_comparables
+
+
+
+#'property_id'	listing_id	rdc_web_url	prop_type	address	branding	prop_status	price	baths_half	baths_full	baths	beds	building_size	agents	office	last_update	client_display_flags	lead_forms	photo_count	thumbnail	page_no	rank	list_tracking	lot_size	mls	virtual_tour	open_houses
+
+
+
+
 post_to_sheets()
